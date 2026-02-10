dabd070300bb0db0307435a0b4ca5b7501f75af1
wisdomfzolutions <wisdom@fzolutions.com>
2026-02-10 08:49:03 +0100

    bug

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -4,16 +4,10 @@ import { clinetRequest } from "../controllers/clinetRequest.js";
 
 const router = express.Router();
 
-/**
- * Save agent push notification token
- * POST /api/notifications/agent
- */
 router.post("/notifications/agent", agentNotification);
 
-/**
- * Create client request + send push notification
- * POST /api/notifications/request
- */
+
+
 router.post("/notifications/request", clinetRequest);
 
 export default router;
